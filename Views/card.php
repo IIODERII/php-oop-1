625ebb5054d9602138898eb817416a1734f9c6a4
IIODERII <oderrisi@gmail.com>
2023-12-04 15:17:55 +0100

    added a darker color to card bg

--- Views/card.php
+++ Views/card.php
@@ -1,5 +1,5 @@
 <div class="col-12 col-md-4 col-lg-3">
-    <div class="card h-100">
+    <div class="card h-100 bg-secondary">
         <img src="<?= $image ?>" class="card-img-top my-ratio" alt="<?= $title ?>">
         <div class="card-body">
             <h5 class="card-title">
